4a1212c64c6195ee6ab87c17faccaa2c7bfb0579
Maisha-sultana <sultanaboishakhi9@gmail.com>
2025-08-28 23:02:07 +0600

    added js changes

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-
 function getElement(id) {
   return document.getElementById(id);
 }
@@ -24,54 +23,68 @@ getElement("product-box").addEventListener("click", function (e) {
   }
 
 //  call
-  if (clickedBtn.className.includes("call-btn")) {
-    const card = clickedBtn.parentNode.parentNode;
+if (clickedBtn.classList.contains("call-btn")) {
+  const card = clickedBtn.closest(".card");
 
-    const serviceName = card.children[1].children[0].innerText;
-    const serviceNumber = card.children[1].children[2].innerText;
+  const serviceName = card.querySelector("h2").innerText;
+  const serviceNumber = card.querySelector("h3").innerText;
 
-    if (coinCount < 20) {
-      alert("Not enough coins to make the call!");
-      return;
-    }
-
-    coinCount = coinCount - 20;
-    coinCountElement.innerText = coinCount;
-
-    alert("Calling " + serviceName + " at " + serviceNumber);
-
-    
-
-
-    const newEntry = document.createElement("div");
-    newEntry.classList.add("bg-gray-100", "rounded-xl", "p-3", "my-2");
-
-    const namePara = document.createElement("p");
-    namePara.innerText = serviceName;
-    namePara.classList.add("font-semibold", "text-md");
-
-    const numberPara = document.createElement("p");
-    numberPara.innerText = serviceNumber;
-    numberPara.classList.add("text-sm", "text-gray-500");
-
-    newEntry.appendChild(namePara);
-    newEntry.appendChild(numberPara);
-    cartContainer.appendChild(newEntry);
+  if (coinCount < 20) {
+    alert("Not enough coins to make the call!");
+    return;
   }
+
+  coinCount -= 20;
+  coinCountElement.innerText = coinCount;
+
+  alert("Calling " + serviceName + " at " + serviceNumber);
+
+  const callTime = new Date().toLocaleTimeString();
+
+  const newEntry = document.createElement("div");
+  newEntry.classList.add("bg-gray-100", "rounded-xl", "p-3", "my-2");
+
+  const flexContainer = document.createElement("div");
+ flexContainer.classList.add("flex", "justify-between", "items-center");
+
+  const namePara = document.createElement("p");
+  namePara.innerText = serviceName;
+  namePara.classList.add("font-semibold", "text-md");
+
+
+  const timePara = document.createElement("p");
+  timePara.innerText =  callTime;
+  timePara.classList.add("text-xs", "text-gray-400");
+
+  flexContainer.appendChild(namePara);
+  flexContainer.appendChild(timePara);
+  
+  
+  const numberPara = document.createElement("p");
+  numberPara.innerText = serviceNumber;
+  numberPara.classList.add("text-sm", "text-gray-500");
+
+  
+ 
+  newEntry.appendChild(flexContainer);
+   newEntry.appendChild(numberPara);
+   
+  cartContainer.appendChild(newEntry);
+
+}
+
 
 // copy
 
 let copyCountElement = document.getElementById("copy-count");
 copyCountElement.innerText = copyCount + " Copy";
-
-if (clickedBtn.className.includes("copy-btn")) {
-  let card = clickedBtn.parentNode.parentNode;
-   serviceNumber = card.children[1].children[2].innerText;
+if (clickedBtn.classList.contains("copy-btn")) {
+  const card = clickedBtn.closest(".card");
+  const serviceNumber = card.querySelector("h3").innerText;
 
   alert("Number " + serviceNumber + " copied!");
 
-
-  copyCount = copyCount + 1;
+  copyCount += 1;
   copyCountElement.innerText = copyCount + " Copy";
 }
 
